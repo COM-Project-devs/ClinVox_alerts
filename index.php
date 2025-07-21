40bc0093024d7bf212f48d2d949ca11adcce617d
Macrdev04 <manuelcomtrabajo@gmail.com>
2025-07-21 13:27:47 -0400

    creación de index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Alertas Sonoras IP</title>
+</head>
+<body>
+    <h1>🔊 Alertas Sonoras</h1>
+    <form method="POST" action="play.php">
+        <button name="audio" value="evacuacion.mp3">🚨 Evacuación General</button>
+        <button name="audio" value="simulacro.mp3">🧪 Simulacro</button>
+        <button name="audio" value="emergencia.mp3">🔥 Emergencia Piso 2</button>
+    </form>
+</body>
+</html>
